64158eeba6343773fd35afa6465bb90c42a2be63
BaeHyeonWoo <qogusdn1017@naver.com>
2021-08-22 01:28:39 +0900

    Version Update
    Tap 4.1.6 / Kommand 2.6.4 / Gradle 7.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,18 +3,18 @@ plugins {
 }
 
 repositories {
-    mavenCentral() // 코드 한줄을 줄여주셨습니다 각별님 감사합니다
-    maven("https://papermc.io/repo/repository/maven-public/") // PaperMC
+    mavenCentral()
+    maven("https://papermc.io/repo/repository/maven-public/")
 }
 
 dependencies {
-    compileOnly(kotlin("stdlib")) // Kotlin
-    compileOnly("io.papermc.paper:paper-api:1.17.1-R0.1-SNAPSHOT") // Paper Latest
-    compileOnly("io.github.monun:tap-api:4.1.1")
-    compileOnly("io.github.monun:kommand-api:2.2.0") // 와 shadowJar 버리는 시대가 올 줄이야
+    compileOnly(kotlin("stdlib"))
+    compileOnly("io.papermc.paper:paper-api:1.17.1-R0.1-SNAPSHOT")
+    compileOnly("io.github.monun:tap-api:4.1.6")
+    compileOnly("io.github.monun:kommand-api:2.6.4")
 }
 
-tasks { // 아니 시발 페이퍼 개놈들이 굳이 꼭 16만 되게 해야할 이유가 뭔데요
+tasks {
     processResources {
         filesMatching("**/*.yml") {
             expand(project.properties)
