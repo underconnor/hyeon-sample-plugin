e7c3da6d21487e734a2b7f29b4d6f537fee99e2e
BaeHyeonWoo <qogusdn1017@naver.com>
2022-02-05 14:12:18 +0900

    :bug: Oops

---
+++ src/main/kotlin/com/baehyeonwoo/sample/plugin/objects/SampleObject.kt
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2022 BaeHyeonWoo
+ *
+ *  Licensed under the General Public License, Version 3.0. (https://opensource.org/licenses/gpl-3.0/)
+ */
+
+package com.baehyeonwoo.sample.plugin.objects
+
+import com.baehyeonwoo.sample.plugin.SamplePluginMain
+
+/***
+ * @author BaeHyeonWoo
+ *
+ * "Until my feet are crushed,"
+ * "Until I can get ahead of myself."
+ */
+
+object SampleObject {
+    val plugin = SamplePluginMain.instance
+
+    val server = plugin.server
+
+    const val message = "Hello World!"
+}
